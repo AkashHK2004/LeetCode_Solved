c625aede4c933ef189b6755a2724d83ee3b90140
Akash H K <136916144+AkashHK2004@users.noreply.github.com>
2023-08-28 18:26:10 +0530

    Time: 16 ms (69.37%), Space: 13.3 MB (50.97%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        st1=str(x)
+        st2=""
+
+        if st1[0]=='-':
+            lst=list(st1)
+            for i in range(1,len(st1)):
+                st2+=lst[len(st1)-i]
+            n=-(int(st2))
+        else:
+            for i in range(1,len(st1)+1):
+                st2+=st1[len(st1)-i]
+            n=int(st2)
+        if n>(-2**31) and n<((2**31)-1):
+            return n
+        else:
+            return 0
+        
+        
